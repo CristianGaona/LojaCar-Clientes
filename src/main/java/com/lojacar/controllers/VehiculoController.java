14c507b505f5d487ea08a908070f39067c4460ed
Roddy Andrés Correa T <ra.correa90@gmail.com>
2020-02-20 16:06:38 -0500

    Pre-release API v1
    Pre-Release  v1

--- src/main/java/com/lojacar/controllers/VehiculoController.java
+++ src/main/java/com/lojacar/controllers/VehiculoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import com.lojacar.models.entity.Vehiculos;
 import com.lojacar.models.service.IVehiculoService;
 
 @RestController
+@RequestMapping("/lojacar/v1")
 public class VehiculoController {
 
 	@Autowired
