e08aae85c62179e6f02b34ca4c9cd11099b4f846
Roddy Andrés Correa T <ra.correa90@gmail.com>
2020-02-20 17:30:41 -0500

    add Cross origin in controllers
    add Cross origin in controllers

--- src/main/java/com/lojacar/controllers/ClienteController.java
+++ src/main/java/com/lojacar/controllers/ClienteController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,7 @@ import com.lojacar.models.service.IEmpleadoService;
 import com.lojacar.models.service.IVehiculoService;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RequestMapping("/lojacar/v1")
 public class ClienteController {
 
