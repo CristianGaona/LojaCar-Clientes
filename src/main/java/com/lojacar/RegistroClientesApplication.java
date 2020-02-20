f0c05b12a6bcd002a5de56d50e1540bdaa8a528a
CristianGaona <crgaonas24@gmail.com>
2020-02-20 09:37:57 -0500

    microservicio de clientes

---
+++ src/main/java/com/lojacar/RegistroClientesApplication.java
@@ -0,0 +1,13 @@
+package com.lojacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojacar/RegistroClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
